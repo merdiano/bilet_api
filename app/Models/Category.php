53244aad6b731516ab7354b7f6c474563c7011d0
merdan <merdan.m@gmail.com>
2019-12-11 13:49:05 +0500

    details fix

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -42,7 +42,7 @@ class Category extends Model
         return $query->select('id','title_tk','title_ru','lft')
             ->orderBy('lft')
             ->with(['events' => function($q) use($limit){
-                $q->select('id','title','description','category_id','sub_category_id','start_date')
+                $q->select('id','title','category_id')
                     ->limit($limit)
 //                    ->with('starting_ticket')
                     ->withCount(['stats as views' => function($q){
