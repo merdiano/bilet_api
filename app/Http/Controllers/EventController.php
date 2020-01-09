4b5dab9a256c2c7a47e960b435f560441a5d6c82
merdan <merdan.m@gmail.com>
2020-01-09 21:15:36 +0500

    get tickets fix

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -60,7 +60,7 @@ class EventController extends Controller
             ->findOrFail($event_id,['id','venue_id']);
 
         $tickets = Ticket::select('id','title','description',"price", "max_per_person", "min_per_person","start_sale_date","end_sale_date","ticket_date","section_id")
-            ->with(['section','reserved:seat_no,ticket_id','booked:seat_no,ticket_id'])
+            ->with(['section:id,section_no,description,seats,section_no_ru,description_ru,section_no_tk,description_tk','reserved:seat_no,ticket_id','booked:seat_no,ticket_id'])
             ->where('event_id',$event_id)
             ->where('ticket_date',$request->get('ticket_date'))
             ->where('is_hidden', false)
