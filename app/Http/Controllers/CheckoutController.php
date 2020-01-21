56bd7ace238d461742d74d2456ca5caff98128bc
merdan <merdan.m@gmail.com>
2020-01-21 17:44:00 +0500

    reserve tickets fix booking fee

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -96,6 +96,8 @@ class CheckoutController extends Controller
             $eventTicket = Ticket::with('event:id,organiser_fee_fixed,organiser_fee_percentage')
                 ->findOrFail($ticket_id);
 
+            dd($eventTicket->booking_fee);
+
             $max_per_person = min($eventTicket->quantity_remaining, $eventTicket->max_per_person);
             /*
              * Validation max min ticket count
