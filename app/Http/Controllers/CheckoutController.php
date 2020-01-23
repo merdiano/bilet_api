f0beda430f696a4290036bcaff5ef23e169f3916
merdan <merdan.m@gmail.com>
2020-01-24 01:47:32 +0500

    checkoutController register order fix test

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -190,7 +190,7 @@ class CheckoutController extends Controller
         $holder_surname = $request->get('surname');
         $holder_email = $request->get('email');
 
-        $event = Event::withReserved($phone_id)
+        $event = Event::withReserved($phone_id)->with('organiser')
             ->findOrFail($event_id,['id','organiser_fee_fixed','organiser_fee_percentage']);
 
         if(empty($event->reservedTickets) || $event->reservedTickets->count() == 0){
