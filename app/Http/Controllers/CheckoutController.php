b9ee8d3a3dfa81d24d03c5a79b3ecfb0c7ba0c18
merdan <merdan.m@gmail.com>
2020-01-24 00:35:43 +0500

    event controller views

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -17,6 +17,7 @@ use App\Services\Order as OrderService;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class CheckoutController extends Controller
 {
