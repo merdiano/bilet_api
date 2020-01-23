635f00fbd62ec20ddaa2c806082623cb34ae8d61
merdan <merdan.m@gmail.com>
2020-01-24 01:06:40 +0500

    event controller views

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -97,9 +97,9 @@ class Event extends  Model
 
     public function scopeWithReserved($query,$phone_id){
         return $query->with(['reservedTickets' => function($q) use ($phone_id){
-            $q->select('id','session_id','ticket_id','event_id','expires');
+            $q->select('id','session_id','ticket_id','event_id','expires')
 //                ->where('session_id',$phone_id );
-//                ->where('expires','>',Carbon::now())
+                ->where('expires','>',Carbon::now());
 //                ->with('ticket:id,price');
         }]) ;
     }
