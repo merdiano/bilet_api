800e2ff18520cc159fad317f83214d4265c9c42d
merdan <merdan.m@gmail.com>
2020-01-27 17:47:51 +0500

    route bug checkincontroller get attendees

--- app/Http/Controllers/CheckinController.php
+++ app/Http/Controllers/CheckinController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 
 
 use App\Models\Attendee;
+use App\Models\Event;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CheckinController extends Controller
 {
@@ -17,8 +19,8 @@ class CheckinController extends Controller
             return response()->json(['status'=>'error','message'=>'ticket_date does not exists'],405);
 
         $ticket_date = $request->get('ticket_date');
-        $attendess = Attendee::select('attendees.id','ticket_id','attendees.first_name','attendees.last_name','attendees.email',
-            'seat_no','reference_index','has_arrived','arrival_time','private_reference_number','orders.order_reference')
+        $attendess = Attendee::select('attendees.id','ticket_id','attendees.first_name','attendees.last_name',
+            'attendees.email', 'seat_no','reference_index','has_arrived','arrival_time','orders.order_reference')
             ->join('tickets', 'tickets.id', '=', 'attendees.ticket_id')
             ->join('orders','orders.id','=','attendees.order_id')
             ->where(function ($query) use ($event_id,$ticket_date) {
@@ -33,5 +35,35 @@ class CheckinController extends Controller
 
     public function checkInAttendees(Request $request, $event_id){
 
+        $event = Event::where('id',$event_id)->where('user_id',$request->auth->id)->first();
+
+        if(!empty($event) && $request->has('attendees')){
+            $checks = json_decode($request->get('attendees'),true);
+            $arrivals = array_column($checks, 'arrival_time', 'id');
+            $att_ids = array_column($checks, 'id');
+            try{
+                DB::beginTransaction();
+                $attendees = Attendee::whereIn('id',$att_ids)->get();
+
+                foreach ($attendees as $attendee){
+                    $attendee->checked_in = true;
+                    $attendee->arrival_time = $arrivals[$attendee->id];
+                    $attendee->save();
+                }
+                DB::commit();
+
+                return response()->json([
+                    'status'=>'success'
+                ]);
+
+            }catch (\Exception $ex){
+                DB::rollBack();
+                return response()->json([
+                    'status'=>'error',
+                    'message' => $ex->getMessage()
+                ]);
+            }
+
+        }
     }
 }
